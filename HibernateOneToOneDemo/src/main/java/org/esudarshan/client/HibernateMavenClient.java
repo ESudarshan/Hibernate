a31417f44fe241d4c29ce1f86fe9db3a4b4d1976
esudarshan <sudarshanekambe@gmail.com>
2018-01-08 18:26:19 +0530

    One To One Mapping

---
+++ HibernateOneToOneDemo/src/main/java/org/esudarshan/client/HibernateMavenClient.java
@@ -0,0 +1,45 @@
+package org.esudarshan.client;
+
+import org.esudarshan.model.User;
+import org.esudarshan.model.Vehicle;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateMavenClient {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome");
+
+		User userOne = new User();
+		userOne.setName("One");
+		Vehicle vehicleOne = new Vehicle();
+		vehicleOne.setName("Car");
+		userOne.setVehicle(vehicleOne);
+
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(userOne);
+		session.save(vehicleOne);
+		session.getTransaction().commit();
+		session.close();
+
+		try {
+			session = sessionFactory.openSession();
+			session.beginTransaction();
+			User user = new User();
+			user = session.get(User.class, 1);
+			System.out.println(user);
+			System.out.println(user.getVehicle());
+			session.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println("Thank you!");
+		}
+	}
+
+}
---
+++ HibernateOneToOneDemo/src/main/java/org/esudarshan/model/User.java
@@ -0,0 +1,53 @@
+package org.esudarshan.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USER_INFO")
+public class User {
+
+	@Id // Primary Key
+	@GeneratedValue(strategy = GenerationType.SEQUENCE) // (AUTO, IDENTITY, SEQUENCE, TABLE)
+	private int id;
+
+	private String name;
+
+	@OneToOne
+	@JoinColumn(name="VEHICLE_ID")
+	private Vehicle vehicle;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+
+	@Override
+	public String toString() {
+		return id + "\t" + name;
+	}
+}
